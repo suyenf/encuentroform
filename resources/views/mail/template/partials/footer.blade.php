60d2aac93b70b290795b475342dfb067b36cc4b3
suyen <suyenfaudito@gmail.com>
2022-10-29 01:47:53 -0400

    estructura de template para email

---
+++ resources/views/mail/template/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<tr style="padding:40px 0 30px 0;text-align:center;">
+    <td style="padding:30px;background:#d2e5f2;">
+
+        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+            <tr style="padding:0;">
+                <td style="padding:0;width:50%;" align="left">
+                    <p>&reg; Nazca Copyright 2022. <br/></p>
+                </td>
+                <td style="padding:0;width:50%;" align="right">
+                    <p>Designed & Powered by<br/><a href="https://itnazca.com/">Nazca Tech &
+                            Consulting</a></p>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
---
+++ resources/views/mail/template/partials/header.blade.php
@@ -0,0 +1,7 @@
+<tr style="text-align:center;">
+    <td align="center" style="padding:40px 0 30px 0;background:#d2e5f2;">
+        <h1>Schoenstatt Houston Family Day</h1>
+        <img alt="" width="130" style="height:auto;display:block;"
+             src="{{asset("storage/uploads/logo.png")}}"/>
+    </td>
+</tr>
